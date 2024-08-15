59f785f195da881482448a623e56cf705004417c
ianrscott2 <ianrscott@protonmail.com>
2024-08-15 13:29:49 -0500

    fix formatting

--- tests/test_newsapi.py
+++ tests/test_newsapi.py
@@ -1,6 +1,7 @@
 import pytest
 from news import News
 
+
 @pytest.fixture
 def example_news_data():
     import os
@@ -8,20 +9,22 @@ def example_news_data():
     API_KEY_NEWS = os.environ["API_KEY_NEWS"]
 
     return [
-    {
-        "api_id": API_KEY_NEWS,
-    }
-]
+        {
+            "api_id": API_KEY_NEWS,
+        }
+    ]
 
-expected = {"status":"ok"}
+
+expected = {"status": "ok"}
+
 
 def test_connect_to_news_data(example_news_data):
     for data_set in example_news_data:
         news_inst = News(170)
-        news_data = news_inst.update(data_set['api_id'])
+        news_data = news_inst.update(data_set["api_id"])
         print(news_data)
-        status_string = str(news_data['status'])
+        status_string = str(news_data["status"])
         print("The status string is: " + status_string)
         print("The expeced status is: " + expected["status"])
-        #assert str("wrong") == str(expected["lat"])
-        assert str(news_data['status']) == str(expected["status"])
+        # assert str("wrong") == str(expected["lat"])
+        assert str(news_data["status"]) == str(expected["status"])
