337a46afa18d381b217458ad1ea03da2f40e7443
Ian R Scott <ianrscott@protonmail.com>
2024-08-15 12:07:56 -0500

    Update waveshare_config.sh

--- waveshare_config.sh
+++ waveshare_config.sh
@@ -1,28 +1,15 @@
 #!/usr/bin/sh
 #
-apt update; apt -y upgrade
-apt install python3-venv
-cd ~/; python -m venv weather_station_venv
-source weather_station_venv/bin/activate
-pip install requests pillow gpiozero pytest;
+#apt update; apt -y upgrade
+apt install python3-pip python3-pil python3-numpy python3-gpiozero
+pip3 install RPi.GPIO spidev
 cd /tmp
 if [ -d '/tmp/waveshare' ]; then rm -Rf /tmp/waveshare; fi
 mkdir -p /tmp/waveshare
 cd waveshare
-wget https://github.com/joan2937/lg/archive/master.zip
-wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.71.tar.gz
-wget https://project-downloads.drogon.net/wiringpi-latest.deb
-git clone https://github.com/WiringPi/WiringPi
-unzip master.zip
-tar zxvf bcm2835-1.71.tar.gz
-cd /tmp/waveshare/lg-master
-make
-make install
-apt install gpiod libgpiod-dev wiringpi
-cd /tmp/waveshare/bcm2835-1.71/
-./configure && make && make check && make install
-dpkg -i wiringpi-latest.deb
-gpio -v
-cd /tmp/waveshare/WiringPi
-./build
-gpio -v
+git clone https://github.com/waveshare/e-Paper.git
+wget https://files.waveshare.com/upload/7/71/E-Paper_code.zip
+unzip E-Paper_code.zip -d e-Paper
+cd e-Paper/RaspberryPi_JetsonNano/
+cd python/examples
+#python3 epd_2in7_V2_test.py
