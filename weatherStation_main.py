d22520c23023c785834d384051536067cfc4112e
ianrscott2 <ianrscott@protonmail.com>
2024-09-10 11:04:50 -0500

    test

--- weatherStation_main.py
+++ weatherStation_main.py
@@ -333,9 +333,10 @@ class weather_station:
                 position = -6
 
             if position != None:
+                print("Labelling with " + int(round(start_h - 4), corner[1] - position) + " and " + round(weather_data))
                 draw.text(
                     (round(start_h - 4), corner[1] - position),
-                    weather_data,
+                    round(weather_data),
                     fill=0,
                     font=font8,
                 )
