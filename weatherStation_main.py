5a283e4f9fb9fd85a186185bab2b95bf40a7caa9
ianrscott2 <ianrscott@protonmail.com>
2024-09-13 14:12:24 -0500

    test

--- weatherStation_main.py
+++ weatherStation_main.py
@@ -637,7 +637,7 @@ class weather_station:
         )  # CURRENT WIND
         wind_dir, wind_deg = self.weather.wind_dir(current_info["wind_deg"])
         if wind_deg != 360:
-            arrow_icon = arrow_icon.rotate(wind_deg, fillcolor=0)
+            arrow_icon = arrow_icon.rotate(wind_deg)
 
         Himage.paste(arrow_icon, (70, 130))
         draw.text((165, 121), "Rain 24h", fill=0, font=font12)
