5d3c299e96e097ee245353db56d84b2b1f6d8a5a
ianrscott2 <ianrscott@protonmail.com>
2024-09-13 15:27:49 -0500

    test

--- weatherStation_main.py
+++ weatherStation_main.py
@@ -654,7 +654,7 @@ class weather_station:
             font=font14,
         )  # CURRENT WIND
         wind_deg = self.weather.curent_wind_deg()
-        arrow_icon = arrow_icon.rotate(angle=wind_deg,fillcolor="#FFFFFF")
+        arrow_icon = arrow_icon.rotate(angle=-wind_deg,fillcolor="#FFFFFF")
         Himage.paste(arrow_icon, (30, 130))
         #draw.text((100, 130), str(wind_deg), fill=0, font=font12)
         draw.text((165, 121), "Rain 24h", fill=0, font=font12)
