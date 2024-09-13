e4729f1493952654330503f17b1db0500abbe55a
ianrscott2 <ianrscott@protonmail.com>
2024-09-13 15:55:18 -0500

    test

--- weatherStation_main.py
+++ weatherStation_main.py
@@ -137,7 +137,7 @@ class weather_station:
 
     def get_date_header(self, draw, title=""):
         draw.text((0, 0), self.weather.current_time(), fill=0, font=font12)
-        draw.text((120, 0), title, fill=0, font=font12)
+        draw.text((180, 0), title, fill=0, font=font12)
         draw.line((0, 15, 264, 15), fill=0, width=1)  # HORIZONTAL SEPARATION
         return draw
 
@@ -653,7 +653,7 @@ class weather_station:
         )  # CURRENT WIND
         wind_deg = self.weather.curent_wind_deg()
         arrow_icon = arrow_icon.rotate(angle=-wind_deg,fillcolor="#FFFFFF")
-        Himage.paste(arrow_icon, (60, 120))
+        Himage.paste(arrow_icon, (60, 130))
         #draw.text((100, 130), str(wind_deg), fill=0, font=font12)
         draw.text((165, 121), "Rain 24h", fill=0, font=font12)
         if "rain" in day_info:
