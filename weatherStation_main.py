1bc827073a39020e729d5ac1de82e68bc14c50de
ianrscott2 <ianrscott@protonmail.com>
2024-09-13 15:08:57 -0500

    test

--- weatherStation_main.py
+++ weatherStation_main.py
@@ -637,11 +637,9 @@ class weather_station:
             font=font14,
         )  # CURRENT WIND
         wind_deg = self.weather.curent_wind_deg()
-        if wind_deg != 360:
-            arrow_icon = arrow_icon.rotate(angle=wind_deg,fillcolor="#FFFFFF")
-
-        Himage.paste(arrow_icon, (70, 130))
-        draw.text((100, 130), str(wind_deg), fill=0, font=font12)
+        arrow_icon = arrow_icon.rotate(angle=wind_deg,fillcolor="#FFFFFF")
+        Himage.paste(arrow_icon, (30, 130))
+        #draw.text((100, 130), str(wind_deg), fill=0, font=font12)
         draw.text((165, 121), "Rain 24h", fill=0, font=font12)
         if "rain" in day_info:
             day_rain = day_info["rain"]
