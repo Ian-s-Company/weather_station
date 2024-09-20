53cc0f605c0f6c200d9bd1b1044e13ca83c7edaf
ianrscott2 <ianrscott@protonmail.com>
2024-09-20 09:05:31 -0500

    update logging format

--- weatherStation_main.py
+++ weatherStation_main.py
@@ -92,8 +92,9 @@ with open(app_dir + "/weatherstation.service", "r") as file:
 with open(app_dir + "/weatherstation.service", "w") as file:
     file.write(data)
 
+FORMAT = '%(asctime)s %(clientip)-15s %(user)-8s %(message)s'
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename=app_dir + "/weatherStation.log", level=logging.DEBUG)
+logging.basicConfig(filename=app_dir + "/weatherStation.log", level=logging.DEBUG, format=FORMAT)
 
 if screen_size == "7x5in":
     logger.info("Screen size is 7x5in")
